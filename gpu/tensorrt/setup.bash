72c98015dc87f4bad8bd61505c9f7a5eb76560b0
hexu <hexu_1985@sina.com>
2024-06-12 17:01:16 +0800

    update gpu/tensorrt

---
+++ gpu/tensorrt/setup.bash
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/TensorRT-8.5.3.1/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/local/TensorRT-8.5.3.1/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/TensorRT-8.5.3.1/include
