2843f19ec1fe27c478cac6a43045caf2406087b8
hexu <hexu_1985@sina.com>
2025-11-28 14:28:43 +0800

    update perf/flamegraph/c

--- perf/flamegraph/c/run_pid.sh
+++ perf/flamegraph/c/run_pid.sh
@@ -1,7 +1,13 @@
-
-rm -f ./perf.data
 nohup ./sched 20000 200 &
 sched_pid=$!
-perf record -F 99 -p ${sched_pid} -g sleep 15 
-#perf report -n --stdio
-perf script | ./stackcollapse-perf.pl > out.perf-folded && ./flamegraph.pl out.perf-folded > perf.svg
+
+mkdir -p perf_result
+mkdir -p perf_fold
+mkdir -p perf_svg
+
+data_file="perf_result/perf.data"
+fold_file="perf_fold/perf.fold"
+svg_file="perf_svg/perf.svg"
+perf record -F 99 -p ${sched_pid} -o "$data_file" -g -- sleep 10
+perf script -i "$data_file" | ./stackcollapse-perf.pl > "$fold_file"
+./flamegraph.pl "$fold_file" > "$svg_file"
